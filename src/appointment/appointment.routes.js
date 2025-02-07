4ef13fc108b361ffcf0c1679c90f42550aaf1da3
Dgarcia-09 <dgarcia-2023532@kinal.edu.gt>
2025-02-07 11:01:57 -0600

    feat: funcion de actualizar datos de la cita agregada

--- src/appointment/appointment.routes.js
+++ src/appointment/appointment.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { saveAppointment , getAppointments} from "./appointment.controller.js";
+import { saveAppointment , getAppointments, updateAppointment} from "./appointment.controller.js";
 import { createAppointmentValidator } from "../middlewares/appointment-validators.js";
 
 const router = Router();
 
 router.post("/createAppointment", createAppointmentValidator, saveAppointment);
 
-router.get("/", getAppointments)
+router.get("/:uid", getAppointments);
+
+router.put("/updtateAppointment/:aid", updateAppointment);
 
 export default router;
