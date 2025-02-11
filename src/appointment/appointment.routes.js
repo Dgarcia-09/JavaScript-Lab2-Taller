689aed98f249faa6b5292a877a5d451563b44723
Dgarcia-09 <dgarcia-2023532@kinal.edu.gt>
2025-02-10 18:35:10 -0600

    feat: seagregaron las validaciones necesarias

--- src/appointment/appointment.routes.js
+++ src/appointment/appointment.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { saveAppointment , getAppointments, updateAppointment, updateStatus} from "./appointment.controller.js";
-import { createAppointmentValidator } from "../middlewares/appointment-validators.js";
+import { createAppointmentValidator, getAppointmentsValidator, updateAppointmentValidator, updateStatusValidator } from "../middlewares/appointment-validators.js";
 
 const router = Router();
 
 router.post("/createAppointment", createAppointmentValidator, saveAppointment);
 
-router.get("/:uid", getAppointments);
+router.get("/:uid", getAppointmentsValidator, getAppointments);
 
-router.put("/updtateAppointment/:aid", updateAppointment);
+router.put("/updtateAppointment/:aid", updateAppointmentValidator, updateAppointment);
 
-router.patch("/cancelAppointment/:aid", updateStatus )
+router.patch("/cancelAppointment/:aid", updateStatusValidator, updateStatus )
 
 
 
--- src/middlewares/appointment-validators.js
+++ src/middlewares/appointment-validators.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { validarCampos } from "./validate-fields.js";
 import { handleErrors } from "./handle-errors.js";
 
@@ -10,3 +10,21 @@ export const createAppointmentValidator = [
     handleErrors
 ];
 
+export const getAppointmentsValidator = [
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    validarCampos,
+    handleErrors
+];
+
+export const updateAppointmentValidator = [
+    param("aid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    body("date").optional().notEmpty().withMessage("Ingrese una fecha"),
+    validarCampos,
+    handleErrors
+];
+
+export const updateStatusValidator = [
+    param("aid").isMongoId().withMessage("Ingrese um Id valido"),
+    validarCampos,
+    handleErrors
+];
